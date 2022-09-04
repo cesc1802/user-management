ddfba378df4f19bc881e689e348dda9e586adc9e
Cesc <thuocnv1802@gmail.com>
2022-09-04 22:39:35 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text font-bold underline">demo tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
